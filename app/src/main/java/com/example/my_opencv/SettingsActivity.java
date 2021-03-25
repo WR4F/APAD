d034512cd9940f24162a5841a2a8a79b25c72cfd
Victor <vroja010@fiu.edu>
2021-03-24 21:45:28 -0400

    2.1: Landscape settings added

--- app/src/main/java/com/example/my_opencv/SettingsActivity.java
+++ app/src/main/java/com/example/my_opencv/SettingsActivity.java
@@ -1,58 +1,33 @@
 package com.example.my_opencv;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    private int selected;
-    private Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
-        selected = 0;
-
-        View b = findViewById(R.id.confirm_b);
-        Button main = (Button) b;
-
-        mainIntent = new Intent(SettingsActivity.this, MainActivity.class);
-
-        main.setOnClickListener(v -> {
-
-            mainIntent.putExtra("flymode", selected);
-            startActivity(mainIntent);
-        });
-
-        Spinner flymode = findViewById(R.id.spinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.flymode_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        flymode.setAdapter(adapter);
-
-        flymode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println((flymode.getItemAtPosition(position)));
-                System.out.println(position);
-
-                selected = position;
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
     }
 }
